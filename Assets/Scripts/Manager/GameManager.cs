ced9ef8688eac33a7babfdb53fc6ec0e995dfa06
CharlieDreemur <zhangwj.charlie@gmail.com>
2023-02-03 23:27:35 -0600

    Add Readme for EventManager

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,10 +1,67 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 
-
-public class EventManager : Singleton<EventManager>
+public class GameManager : Singleton<GameManager>
 {
-    
+    public GameState state = GameState.NotStarted;
+    public GameState State
+    {
+        get { return state; }
+    }
+    void OnEnable() {
+        EventManager.AddEvent("ChangeState", null);
+        EventManager.AddEvent("GameStart", null);
+        EventManager.AddEvent("CorrectChoice", null);
+        EventManager.AddEvent("WrongChoice", null);
+        EventManager.AddEvent("GameOver", null);
+    }
+    void Start()
+    {
+        ChangeState(GameState.Starting);
+    }
+
+    public void ChangeState(GameState newState)
+    {
+        if (newState == state)
+        {
+            return;
+        }
+
+        state = newState;
+        OnStateChanged();
+        switch (newState)
+        {
+            case GameState.Starting:
+                break;
+            case GameState.PlayerTurn:
+                break;
+            case GameState.Correct:
+                break;
+            case GameState.Wrong:
+                break;
+            case GameState.GameOver:
+                break;
+        }
+    }
+
+    private void OnStateChanged()
+    {
+        //StateChanged.Invoke();
+        return;
+    }
+
+}
+
+public enum GameState
+{
+    NotStarted,
+    Starting,
+    PlayerTurn,  // Player input happens here
+    Correct, //Turn when you make correct choices
+    Wrong, //Turn when you make wrong choices
+    GameOver,
 }
