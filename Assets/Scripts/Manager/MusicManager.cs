dfb7d248d95075a6752d004adfd681a731be9c59
CharlieDreemur <zhangwj.charlie@gmail.com>
2023-02-04 05:55:20 -0600

    Implement Naive MainMenu Scene and Music Manager

---
+++ Assets/Scripts/Manager/MusicManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MusicManager : Singleton<GameManager>
+{
+    public List<AudioClip> musicClips = new List<AudioClip>();
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnEnable(){
+        EventManager.AddEvent("UnityStart", new UnityAction(()=>PlayMusic(0))); //When you first enter into the game
+        EventManager.AddEvent("GameStart", new UnityAction(()=>PlayMusic(2))); //When you press the start button
+        EventManager.AddEvent("CorrectChoice", new UnityAction(()=>PlayMusic(3))); //When you make correct choices
+        EventManager.AddEvent("WrongChoice", new UnityAction(()=>PlayMusic(4))); //When you make wrong choices
+        EventManager.AddEvent("GameOver", new UnityAction(()=>PlayMusic(5))); //When you lose
+
+    }
+
+    public void PlayMusic(int index)
+    {
+        if(index >= musicClips.Count)
+            return;
+        audioSource.clip = musicClips[index];
+        audioSource.Play();
+    }
+
+    public void StopMusic()
+    {
+        audioSource.Stop();
+    }
+}
