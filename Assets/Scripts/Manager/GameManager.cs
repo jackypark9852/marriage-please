809f3b2152ab1a8bc3435a8090a0f447ad11fc7c
Roger Wang <rogerwang2520@gmail.com>
2023-02-05 11:24:24 -0600

    REALISTIC SAMPLE TEXT

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,7 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
